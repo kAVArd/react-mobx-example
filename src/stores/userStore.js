b240be1e27b069aabfdc0b7a56bc5232fbf59869
kAVArd <savinovilja1999@gmail.com>
2019-08-19 17:55:18 +0300

    add runInAction utility

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,4 +1,4 @@
-import { observable, action } from 'mobx'
+import { observable, action, runInAction } from 'mobx'
 import axios from 'axios'
 
 const userStore = observable(
@@ -6,16 +6,19 @@ const userStore = observable(
     users: null,
     error: null,
     getUsers () {
-      console.log(`${process.env.REACT_APP_BACKEND_URL}/users`)
       axios.get(`${process.env.REACT_APP_BACKEND_URL}/users`)
         .then(res => res.data)
         .then(
-          action('fetchSuccess', users => {
-            userStore.users = users
-          }),
-          action('fetchError', error => {
-            userStore.error = error
-          })
+          users => {
+            runInAction(() => {
+              userStore.users = users
+            })
+          },
+          error => {
+            runInAction(() => {
+              userStore.error = error
+            })
+          }
         )
     }
   },
